a39353733f64c457658ef60bc0e12ab9b6008edb
mboultoureau <contact@mboultoureau.com>
2021-04-02 10:34:55 +0200

    Ajout des flash

--- html/CodeIgniter/application/views/user/login.php
+++ html/CodeIgniter/application/views/user/login.php
@@ -1,30 +1,3 @@
-<?php
-	$success_msg = $this->session->flashdata('success_msg');
-	$error_msg = $this->session->flashdata('error_msg');
-
-	if ($success_msg) {
-?>
-	<div class="text-center py-4 lg:px-4">
-		<div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-			<span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">Success</span>
-			<span class="font-semibold mr-2 text-left flex-auto"><?php echo $success_msg; ?></span>
-		</div>
-	</div>
-
-<?php 
-	}
-	if ($error_msg) {
-?>
-	<div class="text-center py-4 lg:px-4">
-		<div class="p-2 bg-red-800 items-center text-red-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-			<span class="flex rounded-full bg-red-500 uppercase px-2 py-1 text-xs font-bold mr-3">Error</span>
-			<span class="font-semibold mr-2 text-left flex-auto"><?php echo $error_msg; ?></span>
-		</div>
-	</div>
-<?php
-	}
-?>
-
 <div class="mt-10 flex items-center justify-center">
   <div class="max-w-md w-full space-y-8">
     <div>
--- html/CodeIgniter/application/views/user/register.php
+++ html/CodeIgniter/application/views/user/register.php
@@ -1,18 +1,3 @@
-<?php
-	$error_msg = $this->session->flashdata('error_msg');
-
-	if ($error_msg) {
-?>
-	<div class="text-center py-4 lg:px-4">
-		<div class="p-2 bg-red-800 items-center text-red-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-			<span class="flex rounded-full bg-red-500 uppercase px-2 py-1 text-xs font-bold mr-3">Error</span>
-			<span class="font-semibold mr-2 text-left flex-auto"><?php echo $error_msg; ?></span>
-		</div>
-	</div>
-<?php
-	}
-?>
-
 <div class="mt-10 flex items-center justify-center">
   <div class="max-w-md w-full space-y-8">
     <div>
